ec31b32c43c1ed0c0856d4e066863784bd707b06
KendallRey <kendallreymozo@gmail.com>
2023-03-23 09:28:39 +0800

    added -> account

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Account
 
 # Register your models here.
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('id','email',)
+
+admin.site.register(Account, AccountAdmin)
--- account/models.py
+++ account/models.py
@@ -1,17 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import Group, AbstractUser
-from pos.models import BaseModelWithUUID
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import Group, AbstractUser, User
 
 # Create your models here.
 
-class Account(BaseModelWithUUID):
-
-    user = models.OneToOneField(to=AbstractUser,null=True, blank=True, on_delete=models.CASCADE)
-
-    first_name = models.CharField(max_length=50, verbose_name='First Name')
-    middle_name = models.CharField(max_length=50, blank=True, verbose_name='Middle Name')
-    last_name = models.CharField(max_length=50, verbose_name='Last Name')
-
-    def __str__(self):
-        return f'{self.last_name}, {self.first_name} {self.middle_name}'
+class Account(AbstractUser):
+    class Meta:
+            verbose_name = 'Account'
+            verbose_name_plural = 'Accounts'
