b55ba15f559544f4e6c589dcdbe55ed1b941080b
KendallRey <kendallreymozo@gmail.com>
2023-03-23 13:50:18 +0800

    update -> cart model fields

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Cart
 # Register your models here.
+class CartAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Cart, CartAdmin)
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+from pos.models import BaseModelWithUUID
+from account.models import Account
+
 # Create your models here.
+
+class Cart(BaseModelWithUUID):
+
+    account = models.ForeignKey(to=Account, null=False, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.account.username} Cart'
+
+class CartItem(BaseModelWithUUID):
+
+    cart = models.ForeignKey(to=Cart,null=False,blank=False, on_delete=models.CASCADE)
+    # item = models.ForeignKey(to=Item, verbose_name="Cart Item Reference", null=True, blank=False)
+    quatity = models.IntegerField(verbose_name="Cart Item Quantity", blank=True, null=False, default=1)
+
+    def __str__(self):
+        return f'{self.cart.account.username}'
