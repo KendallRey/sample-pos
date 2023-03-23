e5de6ec13416ca679c23560b23144f610ba33f50
KendallRey <kendallreymozo@gmail.com>
2023-03-23 13:54:01 +0800

    added -> cart item to admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Cart
+from .models import Cart, CartItem
 # Register your models here.
 class CartAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Cart, CartAdmin)
+
+class CartItemAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(CartItem, CartItemAdmin)
