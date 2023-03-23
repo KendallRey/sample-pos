8549edfffb30ec5a87b144d76bcbd455879ce57a
KendallRey <kendallreymozo@gmail.com>
2023-03-23 14:37:04 +0800

    update -> allowed host, use env

--- pos/settings.py
+++ pos/settings.py
@@ -12,11 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import environ
+from dotenv import load_dotenv
 
 # Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,9 +32,9 @@ DEBUG = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-ALLOWED_HOSTS = [
-    '*',
-    ]
+allowed_hosts = os.getenv("ALLOWED_HOSTS").split(',')
+
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
