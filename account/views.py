df9f8cdc0919ce65ee94b42f63d16b937eada0c4
KendallRey <kendallreymozo@gmail.com>
2023-03-27 21:58:11 +0800

    added -> get user api

--- account/views.py
+++ account/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from django.http import JsonResponse, HttpResponse
+from rest_framework.decorators import api_view, permission_classes
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
 
-# Create your views here.
+# @permission_classes([IsAuthenticated])
+# @api_view(['GET'])
+def get_current_user(request):
+    user_details = {}
+    response = HttpResponse()
+    try: 
+        user_details = {
+            "id" : request.user.id,
+            "email" : request.user.email,
+            "username" : request.user.username,
+            "first_name" : request.user.first_name,
+            "last_name" : request.user.last_name,
+        }
+        response.content = user_details
+        response.status_code = 201
+        return JsonResponse(user_details, safe=False)
+    except : 
+        response.status_code = 400
+        return response
