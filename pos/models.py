afe9ff528ccf7f58786c4e65ec68b952019ea6b5
KendallRey <kendallreymozo@gmail.com>
2023-06-20 12:27:00 +0800

    added -> base 64 image serializer

--- pos/models.py
+++ pos/models.py
@@ -5,6 +5,9 @@ from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from safedelete.models import SafeDeleteModel
 from safedelete.models import SOFT_DELETE_CASCADE
+import base64, uuid
+from django.core.files.base import ContentFile
+from rest_framework import serializers
 
 class BaseModelWithUUID(SafeDeleteModel):
     _safedelete_policy = SOFT_DELETE_CASCADE
@@ -20,6 +23,16 @@ class BaseModelWithUUID(SafeDeleteModel):
 
     def hard_delete(self):
         return super(SafeDeleteModel,self).delete()
+
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            # base64 encoded image - decode
+            format, imgstr = data.split(';base64,') # format ~= data:image/X,
+            ext = format.split('/')[-1] # guess file extension
+            id = uuid.uuid4()
+            data = ContentFile(base64.b64decode(imgstr), name = id.urn[9:] + '.' + ext)
+        return super(Base64ImageField, self).to_internal_value(data)
 
 class RequestStatus(models.TextChoices):
     PENDING = 'PENDING', _('Pending')
